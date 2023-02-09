b32e1c81d0845e7ecee64ec6415c7b2ce26b9b56
Ian Guimaraes <ianguimaraes31@gmail.com>
2023-02-09 09:30:29 -0300

    fix: responsiveness adjustments on Summary component

--- src/lib/icons/BarChart.svelte
+++ src/lib/icons/BarChart.svelte
@@ -1,4 +1,4 @@
-<svg width="49" height="48" viewBox="0 0 49 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg width="49" height="48" viewBox="0 0 49 48" fill="none" xmlns="http://www.w3.org/2000/svg" class="{$$props.class}">
     <path d="M36.8334 40V20" stroke="#CBCBCB" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
     <path d="M24.8334 40V8" stroke="#CBCBCB" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
     <path d="M12.8334 40V28" stroke="#CBCBCB" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
--- src/routes/app/expenses/Summary.svelte
+++ src/routes/app/expenses/Summary.svelte
@@ -8,29 +8,29 @@
     export let assetsValue= 15000
 </script>
 
-<div class="grid grid-cols-3 gap-32 font-syne font-600 text-32 tracking-4">
-    <div class="flex flex-col items-center justify-center py-32 rounded-3xl bg-primary">
-        <Book/>
+<div class="grid grid-cols-3 gap-24 tablet:gap-32 font-syne font-600 text-20 tablet:text-32 tracking-4">
+    <div class="flex flex-col items-center justify-center py-24 rounded-3xl bg-transparent tablet:bg-primary">
+        <Book class="h-32 tablet:h-40"/>
         { expenses }
-        <span class="text-24 font-400">
+        <span class="text-16 tablet:text-24 font-600 tablet:font-400">
             expenses
         </span>
     </div>
 
     <div class="flex flex-col items-center justify-center
         py-32
-        rounded-3xl bg-primary shadow-2xl shadow-[0px_3.35px_32px_rgba(99,31,211,0.9)] border border-primary">
-        <Send/>
+        rounded-3xl bg-transparent tablet:bg-primary shadow-2xl tablet:shadow-[0px_3.35px_32px_rgba(99,31,211,0.9)] tablet:border border-primary">
+        <Send class="h-32 tablet:h-40"/>
         $ { expensesValue }
-        <span class="text-24 font-400">
+        <span class="text-16 tablet:text-24 font-600 tablet:font-400">
             per month
         </span>
     </div>
 
-    <div class="flex flex-col items-center justify-center py-32 rounded-3xl bg-primary">
-        <BarChart/>
+    <div class="flex flex-col items-center justify-center py-32 rounded-3xl tablet:bg-primary">
+        <BarChart class="h-32 tablet:h-40"/> 
         $ { assetsValue }
-        <span class="text-24 font-400">
+        <span class="text-16 tablet:text-24 font-600 tablet:font-400">
             in assets
         </span>
     </div>
